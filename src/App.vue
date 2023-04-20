7288c1c92163a97d546c8d8ba7e11fec8be32761
ronnakash <ronnakash@gmail.com>
2023-04-21 00:46:14 +0300

    styles.css file

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,9 @@
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import "@/styles.css";
+import { Vue } from "vue-class-component";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default class App extends Vue {}
+</script>
